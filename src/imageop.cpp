8d49d94cb9bcd3fbd980317827ad41bb7d0fab4d
Pablo <sorianopablo@correo.ugr.es>
2023-10-30 17:06:10 +0100

    Ejercicio1 hecho bien

--- src/imageop.cpp
+++ src/imageop.cpp
@@ -9,12 +9,12 @@
 
 #include <cassert>
 
-Image::Image Crop(int nrow, int ncol, int height, int width) const{
+Image Image::Crop(int nrow, int ncol, int height, int width) const{
     Image aux(height, width);
 
     for(int i = 0; i < height; i++){
         for(int j = 0; j < width; j++){
-            aux.set_pixel(i,j, this.get_pixel(nrow+i,ncol+j));
+            aux.set_pixel(i,j, get_pixel(nrow+i,ncol+j));
         }
     }
 
--- src/subimagen.cpp
+++ src/subimagen.cpp
@@ -3,17 +3,58 @@
  * @brief Programa de la actividad 1: Genera una subimagen a partir de la imagen de un fichero y almacena la subimagen en otro fichero.
  */
 
- #include <iostream>
- #include <cmath>
- #include "../include/image.h"
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
 
- void guardarSubimagen( const char* fich_orig, const char* fich_rdo, int fila, int col, int filas_sub, int cols_sub ){
-    Image imagen, resultado;
+#include <image.h>
 
-    imagen.Load(fich_orig);
+using namespace std;
 
-    resultado = imagen.Crop(fila, col, filas_sub, cols_sub);
+int main (int argc, char *argv[]){
+ 
+  char *origen, *destino; // nombres de los ficheros
+  Image image, subimage;
+  int fila, col, filas_sub, cols_sub;
 
-    resultado.Save(fich_rdo);
+  // Comprobar validez de la llamada
+  if (argc != 7){
+    cerr << "Error: Numero incorrecto de parametros.\n";
+    cerr << "Uso: subimagen <FichImagenOriginal> <FichImagenDestino> <Fila inic> <Col inic> <NumFilas> <NumCols>\n";
+    exit (1);
+  }
 
- }
+  // Obtener argumentos
+  origen  = argv[1];
+  destino = argv[2];
+  fila = atoi(argv[3]);
+  col = atoi(argv[4]);
+  filas_sub = atoi(argv[5]);
+  cols_sub = atoi(argv[6]);
+
+  // Mostramos argumentos
+  cout << endl;
+  cout << "Fichero origen: " << origen << endl;
+  cout << "Fichero resultado: " << destino << endl;
+  cout << "Coordenada fila inicial" << fila << endl;
+  cout << "Coordenada columna inicial" << col << endl;
+  cout << "Número filas subimagen" << filas_sub << endl;
+  cout << "Número columnas subimagen" << cols_sub << endl;
+
+  // Leer la imagen del fichero de entrada
+  if (!image.Load(origen)){
+    cerr << "Error: No pudo leerse la imagen." << endl;
+    cerr << "Terminando la ejecucion del programa." << endl;
+    return 1;
+  }
+
+  // Mostrar los parametros de la Imagen
+  cout << endl;
+  cout << "Dimensiones de " << origen << ":" << endl;
+  cout << "   Imagen   = " << image.get_rows()  << " filas x " << image.get_cols() << " columnas " << endl;
+
+  subimage = image.Crop(fila, col, filas_sub, cols_sub);
+
+  subimage.Save(destino);
+
+}
