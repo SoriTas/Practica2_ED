0ec558d2a96d16929c592e8c6aad520389d2b7e3
manulg48 <lopezmanuel@correo.ugr.es>
2023-10-31 18:02:58 +0100

    Implementacion ejercicio 2

--- src/imageop.cpp
+++ src/imageop.cpp
@@ -22,4 +22,31 @@ Image Image::Crop(int nrow, int ncol, int height, int width) const{
 }
 
 Image::Image Image Zoom2X() const{
-    
+  Image aux;
+  int rows = get_rows;
+  int cols = get_cols;
+  byte interpol1[rows * 2 - 1][cols * 2 - 1]
+  Image aux(rows * 2 - 1,cols * 2 - 1);
+  for (int i = 0;i < rows;i++){
+    if (i % 2 == 0){
+      for (int j = 0;j < cols * 2 - 1;j++){
+         if (j % 2 != 0) 
+          interpol1[i][j] = (interpol1[i][j - 1] + interpol1[i][j + 1]) / 2;
+      }
+    }
+  }
+  for (int i = 0;i < rows * 2 - 1;i++){
+    if (i % 2 != 0){  
+      for (int j = 0;j < cols * 2 - 1;j++){
+          interpol1[i][j] = (interpol1[i - 1][j] + interpol1[i + 1][j]) / 2;
+      }
+    }
+  }
+  for (int i = 0;i < rows * 2 - 1;i++){
+      for (int j = 0;j < cols * 2 - 1;j++){
+        aux.set_pixel(i,j,round(interpol1[i][j]));
+      }
+    }
+  }
+}
+
--- src/zoom.cpp
+++ src/zoom.cpp
@@ -5,3 +5,24 @@
 
 using namespace std;
 
+int main(int argc, char *argv[]){
+  if (argc != 5){
+    throw out_of_range("Invalid arguments!");
+  }
+  
+  char * fich_orig = argv[1];
+  char * fich_rdo = argv[2];
+  int fila = atoi(argv[3]);
+  int columna = atoi(argv[4]);
+  int lado = atoi(argv[5]);
+  Image zoom;
+  
+  if(!image.Load(fich_orig)){
+    cerr << "Error: No pudo leerse imagen";
+  }
+
+  zoom = image.Zoom2x();
+  zoom.crop(fila,columna,lado,lado);
+
+  subimage.Save(fich_rdo);
+}
